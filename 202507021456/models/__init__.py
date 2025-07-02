50e8e08f59603aa299c749fd1211202912dd5244
Paul <66887666+iRedPaul@users.noreply.github.com>
2025-07-02 16:40:58 +0200

    Delete 202507021456 directory

--- 202507021456/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# gui/__init__.py
-"""GUI-Paket für die Hotfolder-Anwendung"""
-
-# core/__init__.py
-"""Core-Funktionalität für die Hotfolder-Anwendung"""
-
-# models/__init__.py
-"""Datenmodelle für die Hotfolder-Anwendung"""
